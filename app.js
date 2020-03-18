d9810e44b8b9f7b2112baa222a0813994f7c964f
oldmo860617 <oldmo860617@gmail.com>
2020-03-18 15:43:21 +0800

    add date string

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ bot.on('message', async function (event) {
 });
 
 // Bot所監聽的webhook路徑與port
-bot.listen('/linewebhook', process.env.PORT || 3000, async function () {
+bot.listen('/linewebhook', process.env.PORT || 3002, async function () {
 	console.log('[BOT已準備就緒]');
 });
--- utils/requestHandler.js
+++ utils/requestHandler.js
@@ -15,8 +15,9 @@ async function requestHandler(requestText) {
 		case "即將上映":
 			const mappedDataFuture = [];
 			const willReleasedMoviesData = await willReleasedMovies();
+
 			willReleasedMoviesData.map((movie) => {
-				mappedDataFuture.push(`${movie.title} \n ${movie.url} \n\n`)
+				mappedDataFuture.push(`${movie.title} \n ${movie.url} \n上映日期: ${movie.date ? movie.date : '尚無資料'}\n\n`)
 			})
 
 			// linebot 有限制回傳長度，因此太長的陣列要做處理 
@@ -27,7 +28,7 @@ async function requestHandler(requestText) {
 
 			return [responseText, responseText2];
 		default:
-			return "你好！我是 Kyle 電影報馬仔～ \n 輸入以下關鍵字以獲得最新電影訊息: \n\n '本週新片' -> 獲取本週新上映電影訊息 \n '即將上映' -> 獲取近期即將上映電影訊息"
+			return "你好！\n我是 Kyle 電影報馬仔～ \n\n 輸入以下關鍵字以獲得最新電影訊息: \n\n 本週新片 -> 獲取本週新上映電影訊息 \n 即將上映 -> 獲取近期即將上映電影訊息"
 	}
 }
 
